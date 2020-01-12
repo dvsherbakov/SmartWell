4cadb0f78122b7d0461c58b30340c2a8331baa57
Dmitry Sherbakov <dvsherbakov@gmail.com>
2020-01-12 20:47:02 +0500

    probe volumes

--- SmartWell/MainWindow.xaml.cs
+++ SmartWell/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Media;
 using System.Linq;
 using System.Collections.Generic;
+using System.Windows.Controls;
 
 namespace SmartWell
 {
@@ -54,6 +55,46 @@ namespace SmartWell
             var w1 = pl.GetByIndex(((KeyValuePair<int, string>)CbCasingShoe.SelectedItem).Key).GetDOut();
             var w2 = pl.GetByIndex(((KeyValuePair<int, string>)CbCasingPipe.SelectedItem).Key).GetDOut();
             var w3 = pl.GetByIndex(((KeyValuePair<int, string>)CbCasingLiner.SelectedItem).Key).GetDOut();
+            var MaxDiam = (new double[] { w1, w2, w3 }).Max();
+            var fHeight = MainWinsowDataContext.CasingShoeLenght + MainWinsowDataContext.CasingPipeLenght + MainWinsowDataContext.CasingLinerLenght;
+            var dx = sWidth / 3 / MaxDiam;
+            var dy = sHeight / fHeight;
+
+            var rect = new System.Windows.Shapes.Rectangle
+            {
+                Stroke = new SolidColorBrush(Colors.Blue),
+                StrokeThickness = 2,
+                Fill = new SolidColorBrush(Colors.DarkBlue),
+                Width = dx * w1,
+                Height = MainWinsowDataContext.CasingShoeLenght * dy
+            };
+            Canvas.SetLeft(rect, sWidth/2 - dx * w1/2);
+            Canvas.SetTop(rect, 0);
+            gPict.Children.Add(rect);
+
+            var rect2 = new System.Windows.Shapes.Rectangle
+            {
+                Stroke = new SolidColorBrush(Colors.Green),
+                StrokeThickness = 2,
+                Fill = new SolidColorBrush(Colors.DarkGreen),
+                Width = dx * w2,
+                Height = MainWinsowDataContext.CasingPipeLenght * dy
+            };
+            Canvas.SetLeft(rect2, sWidth / 2 - dx * w2 / 2);
+            Canvas.SetTop(rect2, MainWinsowDataContext.CasingShoeLenght * dy);
+            gPict.Children.Add(rect2);
+
+            var rect3 = new System.Windows.Shapes.Rectangle
+            {
+                Stroke = new SolidColorBrush(Colors.Red),
+                StrokeThickness = 2,
+                Fill = new SolidColorBrush(Colors.DarkRed),
+                Width = dx * w3,
+                Height = MainWinsowDataContext.CasingLinerLenght * dy
+            };
+            Canvas.SetLeft(rect3, sWidth / 2 - dx * w3 / 2);
+            Canvas.SetTop(rect3, MainWinsowDataContext.CasingShoeLenght * dy + MainWinsowDataContext.CasingPipeLenght * dy);
+            gPict.Children.Add(rect3);
             //var wMax = new int[] { w1, w2, w3 }.Ma
         }
 
